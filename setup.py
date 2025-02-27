2bc8e27b3b4f8c1804dae2855c55538150e7def2
nishajaiswal03 <jaiswalnisha424@gmail.com>
2025-02-27 17:51:55 +0530

    requirements & installation done

---
+++ setup.py
@@ -0,0 +1,51 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Text-Summarizer-Project"
+AUTHOR_USER_NAME = "nishajaiswal03"
+SRC_REPO = "textSummarizer"
+AUTHOR_EMAIL = "jaiswalnisha424@gmail.com"
+
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for NLP app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    packages=setuptools.find_packages(),
+    install_requires=[ 
+        "transformers",
+        "datasets",
+        "sacrebleu",
+        "rouge-score",
+        "py7zr",
+        "pandas",
+        "nltk",
+        "tqdm",
+        "PyYAML",
+        "matplotlib",
+        "torch",
+        "notebook",
+        "boto3",
+        "mypy-boto3-s3",
+        "python-box==6.0.2",
+        "ensure==1.0.2",
+        "fastapi==0.78.0",
+        "uvicorn==0.18.3",
+        "Jinja2==3.1.2",
+    ],
+
+)
