71b3f2eb98ebe24647236b95ad0c77e57bf48729
nishajaiswal03 <jaiswalnisha424@gmail.com>
2025-03-11 11:12:39 +0530

    Data Validation Completed

--- src/textSummarizer/entity/config_entity.py
+++ src/textSummarizer/entity/config_entity.py
@@ -6,3 +6,11 @@ class DataIngestionConfig:
     root_dir: str
     local_data_file: str
     unzip_dir: str
+
+
+@dataclass(frozen=True)
+class DataValidationConfig:
+    root_dir: str
+    STATUS_FILE: str
+    ALL_REQUIRED_FILES: list
+
